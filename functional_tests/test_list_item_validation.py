7ed4afca5a18fb5b69bb92c5e2517d0a65b477d5
Kevin Huang <huangkevin96@gmail.com>
2021-01-13 20:29:41 -0500

    Move unit tests into a folder with single file

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,5 +5,12 @@ from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
-        self.fail('write me!')
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
+        
+        self.fail('finish the test!')
