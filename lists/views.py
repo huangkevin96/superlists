e7dc8de68ca61cbfacf8e29177eb64fa46a9afa2
Kevin Huang <huangkevin96@gmail.com>
2021-01-18 11:57:26 -0500

    Use get_absolute_orl on List model to DRY urls in views

--- lists/views.py
+++ lists/views.py
@@ -16,7 +16,7 @@ def view_list(request, list_id):
         try:
             item.full_clean()
             item.save()
-            return redirect(f'/lists/{list_.id}/')
+            return redirect(list_)
         except ValidationError:
             item.delete()
             error = "You can't have an empty list item"
@@ -33,9 +33,6 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {'error':error})
-    return redirect(f'/lists/{list_.id}/')
-
-# def add_item(request, list_id):
-#     list_ = List.objects.get(id=list_id)
-#     Item.objects.create(text=request.POST['item_text'], list=list_)
-#     return redirect(f'/lists/{list_.id}/')
+#     return redirect('view_list', list_.id)
+    return redirect(list_)
+    #return redirect(f'/lists/{list_.id}/')
